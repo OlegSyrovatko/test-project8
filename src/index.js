ddbb951b085a9b81f978bd0323ad7c1988ae6d25
Oleg <112899765+OlegSyrovatko@users.noreply.github.com>
2023-03-24 17:04:37 +0200

    test

--- src/index.js
+++ src/index.js
@@ -1,6 +1,17 @@
-
+import datepicker from 'js-datepicker'
 const datepicker = require('js-datepicker');
 
-let options = "";
+let options = {
+    onSelect: instance => {
+        // Show which date was selected.
+        console.log(instance.dateSelected)
+    },
+    formatter: (input, date, instance) => {
+        // const value = date.toLocaleDateString();
+        const day = date.getMonth() + 1;
+        input.value = day // => '1/1/2099'
+    }
+};
 const picker = datepicker(document.querySelector('#datepicker'), options);
+
 
