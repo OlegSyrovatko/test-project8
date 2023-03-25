c87f8fc72db04cc872f26b56035f5a7a8e3ee775
Oleg <sirov@ukr.net>
2023-03-25 21:51:12 +0200

    calendar

--- src/index.js
+++ src/index.js
@@ -1,17 +1,2 @@
-import datepicker from 'js-datepicker'
-const datepicker = require('js-datepicker');
-
-let options = {
-    onSelect: instance => {
-        // Show which date was selected.
-        console.log(instance.dateSelected)
-    },
-    formatter: (input, date, instance) => {
-        // const value = date.toLocaleDateString();
-        const day = date.getMonth() + 1;
-        input.value = day // => '1/1/2099'
-    }
-};
-const picker = datepicker(document.querySelector('#datepicker'), options);
-
-
+import './js/calendar.js';
+import './js/api-weather';
